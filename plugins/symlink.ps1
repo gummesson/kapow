0181cba242e588a01d89c8923612f95e2c13cf30
Ellen Gummesson <ellengummesson@gmail.com>
2013-09-01 13:42:53 +0200

    Add 'symlink' plugiin.

---
+++ plugins/symlink.ps1
@@ -0,0 +1,26 @@
+# == Kapow! ====================================================================
+#
+#  Symlink plugin
+#   For creating symbolic links.
+#
+#  Usage:
+#   symlink [SOURCE] [TARGET]
+#
+# ==============================================================================
+
+# == Aliases ===================================================================
+
+Set-Alias symlink Add-SymbolicLink
+
+# == Functions =================================================================
+
+Function Add-SymbolicLink {
+  Param([Parameter(Position=0, Mandatory=$true)]
+        [Alias("s")]
+        [String]$Source,
+        [Parameter(Position=1, Mandatory=$true)]
+        [Alias("t")]
+        [String]$Target)
+  $Link = "/c mklink $Target $Source"
+  Start-Process cmd.exe -ArgumentList "$Link" -Verb "RunAs" 
+}
